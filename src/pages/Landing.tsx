e4b9472b9c61f2e55ee23c324daed1571494a504
Atilla Cantay Gül <atillacantay@hotmail.com>
2021-12-11 17:07:22 +0300

    Landing Page minor changes

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,9 +1,14 @@
+import { styled } from "@mui/system";
 import { useTranslation } from "react-i18next";
+
+const LandingContainer = styled("div")(({ theme }) => ({
+  paddingTop: theme.spacing(4),
+}));
 
 const Landing = () => {
   const { t } = useTranslation();
 
-  return <div>Landing</div>;
+  return <LandingContainer>{t("Landing Page")}</LandingContainer>;
 };
 
 export default Landing;
